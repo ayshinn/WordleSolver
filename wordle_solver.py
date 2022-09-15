39785050a846141e686d16d394aee775a5bdc34e
Anthony Shinn <anthony.shinn48@gmail.com>
2022-09-15 11:51:02 -0700

    Cleans up code comments, and does some optimization on word suggestions

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,17 +1,13 @@
 """
 ### HOW TO USE ###
 On the command line, you'd run
-`python3 CurseWordCalculator.py`
+`python3 wordle_solver.py`
 and it'll prompt you:
-`What is the author? (Only first letter capitalized):`
+`Guess a five letter word:`
 and
-`What is the song name? (all lower case, replace spaces with '-'):`.
+`Results:`.
 
-It'll generate a URL for genius.com, grab lyrics, and then parse through them for any matches to bad words.
-
-(I personally haven't used jupyter but it should run in there I think?
-You may need to figure how to do that, copy this code in a notebook or something)
-
+For help, type "h" or "help" at any point.
 """
 
 def isvalidguess(guess):
@@ -34,7 +30,7 @@ def help():
 def takeguess():
   guess = input("Guess a five letter word: ")
   while not isvalidguess(guess):
-    if guess == "help":
+    if guess == "help" or guess == "h":
       help()
     guess = input("Please guess a word with five letters: ")
 
@@ -43,13 +39,48 @@ def takeguess():
 def submitresults():
   guess_result = input("Results: ")
   while not isvalidresult(guess_result):
-    if guess_result == "help":
+    if guess_result == "help" or guess_result == "h":
       help()
     guess_result = input("Results should only be made up of X/I/O and be 5 characters long: ")
   return guess_result
 
+def showbestremainingwords(words):
+  if len(words) < 20:
+    print(words)
+    return
+
+  # TODO optimize this function to take in known letter positions and only optimize for missing letter spots
+  letterfrequencydict = {}
+  for word in words:
+    word = word.strip()
+    for letter in word:
+      if letter in letterfrequencydict:
+        letterfrequencydict[letter] += 1
+      else:
+        letterfrequencydict[letter] = 1
+
+  remainingwordswithscores = []
+  for word in words:
+    word = word.strip()
+    uniquelettersinword = set(word)    
+    score = 0
+
+    for letter in uniquelettersinword:
+      score += letterfrequencydict[letter]
+
+    if len(remainingwordswithscores) < 20:
+      remainingwordswithscores.append([word, score])
+    else:
+      remainingwordswithscores.sort(key = lambda x: x[1], reverse = True)
+      if score > remainingwordswithscores[-1][1]:
+        remainingwordswithscores.pop()
+        remainingwordswithscores.append([word, score])
+
+  print("Some of the valid words remaining: " + str(remainingwordswithscores) + "\n")
+  return
+
 def main():
-  # Using readlines()
+  # Reads in the list of 5 letter words available to us.
   all_words_file = open('all_words.txt', 'r')
   words = all_words_file.readlines()
   all_words_file.close
@@ -58,20 +89,19 @@ def main():
   for word in words:
     word = word.strip()
 
-  # Step 2: get web URL from input
+  # Prompts the user for their guess and the corresponding result
   print("Welcome to Wordle Solver! First, please input your first guess")
   guess = takeguess()
-
   print("Thanks. Now submit the results from the game.")
   guess_result = submitresults()
 
-  # Time to start filtering and stuff
   notinword = set()
   inword = set()
   knownletters = ['' for i in range(5)]
   knownwrongplacements = [[] for i in range(5)]
 
   while guess_result != "OOOOO":
+    # Gather information based on results on letter placement, and if each letter is in the word or not
     potentiallynotinword = []
     for i in range(5):
       if guess_result[i] == "O":
@@ -87,14 +117,9 @@ def main():
       if letter not in inword:
         notinword.add(letter)
 
-    # FILTER ON MAIN LIST
     prevvalidwordscount = len(words)
 
-    # print("not in word: " + str(notinword))
-    # print("in word: " + str(inword))
-    # print("known letters: " + str(knownletters))
-    # print("known wrong placements: " + str(knownwrongplacements))
-
+    # Filters down the list of words that can possibly remain to guess.
     # filter first on correct letter position words
     for i in range(len(knownletters)):
       if knownletters[i] != '':
@@ -117,14 +142,9 @@ def main():
     newvalidwordscount = len(words)
     print("Number of possible words reduced from " + str(prevvalidwordscount) + " down to " + str(newvalidwordscount) + ".")
 
-    someremainingwords = []
-    for i in range(min(newvalidwordscount, 12)):
-      someremainingwords.append(words[i].strip())
-    print("Some of the valid words remaining: " + str(someremainingwords) + "\n")
+    showbestremainingwords(words)
 
     guess = takeguess()
-
-    print("Thanks. Now submit the results from the game.")
     guess_result = submitresults()
   
   print("Congrats! The word is " + guess)
